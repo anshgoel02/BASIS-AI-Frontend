490456daec616dbec60e2fd26b5e0f010904a034
Ansh Goel <goelansh164@gmail.com>
2025-10-05 20:53:01 +0530

    Changed the baseURL to render url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://basis-ai-backend.onrender.com",
-    baseURL: "http://localhost:10000"
+    baseURL: "https://basis-ai-backend.onrender.com",
+    // baseURL: "http://localhost:10000"
 });
 
 export default api;
